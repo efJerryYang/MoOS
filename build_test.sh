99d026bc18fc08c324dff7a26c3c9f72d4f6fbc1
Jerry Yang <efjerryyang@outlook.com>
2023-05-19 22:53:52 +0800

    chore: update utils to build and run tests

--- build_test.sh
+++ build_test.sh
@@ -7,3 +7,12 @@ rm -rf asm/ bin/ libulib.a
 rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake riscv64/ riscv32/
 
 cmake -DARCH=riscv64 . && make
+
+mkdir ../../../os/testbin
+mkdir ../../../os/testbin/mnt
+touch ../../../os/testbin/mnt/test_mount
+
+cp -a riscv64/* ../../../os/testbin/
+cp -a src/oscomp/*.txt ../../../os/testbin/
+
+echo "Done."
