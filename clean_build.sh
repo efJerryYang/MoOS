10ccc029cf48589cc6514c600fbbb9c8b37d9134
Jerry Yang <efjerryyang@outlook.com>
2023-05-19 23:00:28 +0800

    chore: clean up

--- clean_build.sh
+++ clean_build.sh
@@ -5,3 +5,5 @@ cd testsuits-for-oskernel/riscv-syscalls-testing/user/
 rm -rf build/
 rm -rf asm/ bin/ libulib.a
 rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake riscv64/ riscv32/
+
+echo 'Done."
