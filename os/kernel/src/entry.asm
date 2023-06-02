a7a1b5d1f427ee5fc86d7521bef7d056e2fdb0fc
Metheus <lujunran@foxmail.com>
2023-06-02 18:23:41 +0800

    tried to add boot stack but failed

--- os/kernel/src/entry.asm
+++ os/kernel/src/entry.asm
@@ -2,12 +2,16 @@
     .section .text.entry
     .globl _start
 _start:
+	#stacktop- id*64KB
+
     la sp, boot_stack_top
+	# slli a0,a0,16
+	# sub sp,sp,a0
     call rust_main
 
     .section .bss.stack
     .globl boot_stack
 boot_stack:
-    .space 4096 * 16
+    .space 4096 * 32
     .globl boot_stack_top
 boot_stack_top:
